4ac398f3bee90e66732d8f329d8b7386f1bb8769
metalcow <35390496+metalcow@users.noreply.github.com>
2018-10-27 20:03:34 -0500

    refix

--- src/org/usfirst/frc/team4213/robot/RobotDashboard.java
+++ src/org/usfirst/frc/team4213/robot/RobotDashboard.java
@@ -2,15 +2,8 @@ package org.usfirst.frc.team4213.robot;
 
 import java.util.logging.Logger;
 
-import org.usfirst.frc.team4213.lib14.PDController;
-import org.usfirst.frc.team4213.systems.Climber;
-import org.usfirst.frc.team4213.systems.DriveTrain;
-import org.usfirst.frc.team4213.systems.Elevator;
-
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotDashboard {
@@ -29,23 +22,22 @@ public class RobotDashboard {
 		passLine, rightSwitch, leftSwitch, switchMySide, switchEitherSide, scaleMySide, leftScale, rightScale, scaleOrSwitch, switchOrScale
 	};
 
-
 	private PowerDistributionPanel pdp;
 	private DriverStation driverStation;
 
 	public void initializeDashboard() {
-		//pdp = new PowerDistributionPanel();
+		// pdp = new PowerDistributionPanel();
 		// CameraServer.getInstance().startAutomaticCapture();
 		SmartDashboard.putBoolean("Auto Position For Second Cube", RobotMap.Autonomous.SecondaryCube);
 	}
 
-
 	public void pushGyro() {
-		//SmartDashboard.putNumber("Gyro Reading", DriveTrain.getInstance().getAngle());
+		// SmartDashboard.putNumber("Gyro Reading",
+		// DriveTrain.getInstance().getAngle());
 	}
-	
+
 	public boolean doSecondaryMission() {
 		return SmartDashboard.getBoolean("Auto Position For Second Cube", RobotMap.Autonomous.SecondaryCube);
 	}
-	
+
 }
